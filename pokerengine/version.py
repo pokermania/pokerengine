cd2a498bae8787ace1124c9d89802f61105f34f4
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-03-24 18:10:45 +0000

    version_to check
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@969 c4f34931-41c7-4605-b615-9bb9f479c57e

--- pokerengine/version.py
+++ pokerengine/version.py
@@ -115,7 +115,7 @@ class Version:
             if match:
                 ( version_from, version_to ) = map(Version, match.groups())
                 if ( ( version_from >= current_version and version_from < desired_version )
-                     ( version_to > current_version and version_to <= desired_version ) ):
+                     and ( version_to > current_version and version_to <= desired_version ) ):
                     upgrade_matrix.setdefault(version_from, {})
                     if upgrade_matrix[version_from].has_key(version_to):
                         print "Version: duplicate upgrade string (%s => %s) keep %s, ignore %s" % ( version_from, version_to, upgrade_matrix[version_from][version_to], string)
