ce04404237d7d53dbcba6a446ec78878c96b8c4b
Andor Goetzendorff <goetzend@in.tum.de>
2011-07-31 03:17:33 +0200

    readded if self.max_players=10, needed for tests

--- pokerengine/pokergame.py
+++ pokerengine/pokergame.py
@@ -623,6 +623,8 @@ class PokerGame:
             self.seats_left = [1, 2, 3, 4, 5, 6, 7, 8]
         elif self.max_players == 9:
             self.seats_left = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        elif self.max_players == 10:
+            self.seats_left = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         else:
             self.seats_left = []
             
