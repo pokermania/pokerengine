aa53c7cf8ffd4f1d1338dd95a3b715f0fe71cabb
Hannes Uebelacker <hannes@pokermania.de>
2012-11-14 01:05:25 +0100

    updated engine build

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+from os.path import basename
+from glob import glob
 from distutils.core import setup
 
 from distutils.command.build import build as DistutilsBuild
@@ -9,14 +11,25 @@ from distutils.command.build import build as DistutilsBuild
 class ExtendedBuild(DistutilsBuild):
     
     def run(self):
-        DistutilsBuild.run(self)
-        os.system("make -C po all")
+        os.system("make -C po")
         os.system("conf/build.sh")
+        DistutilsBuild.run(self)
+
+from pprint import pprint as pp
 
 setup(
-    name = 'poker-engine',
-    version = '1.5.3',
-    packages = ['pokerengine'],
-    cmdclass = {'build': ExtendedBuild}
+    name='poker-engine',
+    version='1.5.4',
+    packages=['pokerengine'],
+    data_files=[
+        ('bin', ['pokerconfigupgrade']),
+        ('share/poker-engine/conf', glob('conf/*.xml')),
+        ('share/man/man8', ['pokerconfigupgrade.8']),
+        ('share/doc/python-poker-engine', ['AUTHORS', 'README'])
+    ] + [(
+        'share/locale/%s/LC_MESSAGES' % locale,
+        ['locale/%s/LC_MESSAGES/poker-engine.mo' % locale]
+    ) for locale in list(basename(po).rsplit('.', 1)[0] for po in glob('po/*.po'))],
+    cmdclass={'build': ExtendedBuild }
 )
 
